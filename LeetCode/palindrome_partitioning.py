ec6a48855ba0703064ac3970279c0d25d8a82d5f
jasonfeng1989 <jasonfeng19890915@hotmail.com>
2014-02-09 22:41:17 -0800

    complete palindrome_partitioning_q1

--- LeetCode/palindrome_partitioning.py
+++ LeetCode/palindrome_partitioning.py
@@ -13,23 +13,47 @@
         Output: 1, (aa|b)
 '''
 
-def is_palindrome(string, string_map):
-    if string in string_map:
-        return string_map[string]
-    start = 0
-    end = len(string)-1
+
+'''
+The classic recursion structure to enumerate all the possible set cuts
+def foo(results, one_result, array, start): 
+    if start < s.size():
+        size_t pos = start
+    while pos < s.size():
+        one_result.append
+        foo(result, one_result, s, pos + 1);
+        one_result.pop
+        pos+=1
+    else:
+        got one solution
+    
+'''
+
+from copy import deepcopy
+def is_palindrome(string, start, end):
     while start < end:
         if string[start] != string[end]:
-            string_map[string] = False
             return False
         start += 1
         end -= 1
-    string_map[string] = True
     return True
 
 
-def palindrom_partition_q1(s):
-    palindrom_partition_q1_helper(s, {}, [])
+def palindrome_partition_q1(s):
+    return palindrome_partition_q1_helper(s, 0, [],[])
     
-def palindrome_partition_q1_heler(s,string_map, results):
-    
+def palindrome_partition_q1_helper(s,start, curr_result, results):
+    if start >= len(s):
+        results.append(deepcopy(curr_result))
+        return results
+    for end in range(start, len(s)):
+        if is_palindrome (s, start, end):
+            curr_result.append(s[start:end+1])
+            palindrome_partition_q1_helper(s,start+1, curr_result, results)
+            curr_result.pop()
+    return results
+     
+if __name__=='__main__':
+    test_cases = ['aaa', 'bcddeffeh', 'aab']
+    for each_test_case in test_cases:
+        print each_test_case, palindrome_partition_q1(each_test_case)
