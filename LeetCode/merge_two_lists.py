63ee8b1eda78d90961f189872f830c77eef59325
jasfeng <jasfeng@cisco.com>
2014-03-10 15:14:59 -0700

    add recursive solution to merge_two_lists in leetcode

--- LeetCode/merge_two_lists.py
+++ LeetCode/merge_two_lists.py
@@ -5,7 +5,8 @@
 
 from LinkList import LinkList
 
-def merge_two_lists(listA, listB):
+# iterative
+def merge_two_lists_1(listA, listB):
     p = listA.head.next
     q = listB.head.next
     if not p:
@@ -28,10 +29,30 @@ def merge_two_lists(listA, listB):
         r.next = q
     return result_list
 
+# recursive
+def merge_two_lists_2(list_1, list_2):
+    result = LinkList()
+    result.head.next = merge_two_lists_helper(list_1.head.next, list_2.head.next)
+    return result
+    
+def merge_two_lists_helper(head_1, head_2):
+    if not head_1:
+        return head_2
+    if not head_2:
+        return head_1
+    if head_1.data < head_2.data:
+        ret = head_1
+        ret.next = merge_two_lists_helper(head_1.next, head_2)
+    else:
+        ret = head_2
+        ret.next = merge_two_lists_helper(head_1, head_2.next)
+    return ret
+    
 if __name__=='__main__':
     test_cases = [([1,3,5,6],[2,4]), ([1,3,5],[]), ([1,2,3,4],[5,6,7,8])]
     for each_test_case in test_cases:
         listA_list,listB_list=each_test_case
         listA = LinkList(listA_list)
         listB = LinkList(listB_list)
-        merge_two_lists(listA, listB).print_list()
+        merge_two_lists_1(listA, listB).print_list()
+        merge_two_lists_2(listA, listB).print_list()
