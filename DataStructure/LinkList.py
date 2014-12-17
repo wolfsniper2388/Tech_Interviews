fd338475f5e831cba1b3018b60db501e88cfe9ec
jasfeng <jasfeng@cisco.com>
2014-12-17 15:50:30 -0800

    rewrite LinkList.py, remove dummy head from its own implementation add recursive merge to sort_list.py in LeetCode

--- DataStructure/LinkList.py
+++ DataStructure/LinkList.py
@@ -12,67 +12,78 @@ class ListNode(object):
 class LinkList(object):
     'A Linked list class that supports operation of adding, deleting ListNodes'
     def __init__(self, seq=None):
-        self.head=ListNode(0)
+        self.head = None
         if seq:
             for data in seq:
-                self.append_node(data)
+                self.append_node(ListNode(data))
     
     def __repr__(self):
         return '%s(%r)' %(self.__class__.__name__, self.head)
     
     def __len__(self):
-        p=self.head.next
-        length=0
+        p = self.head
+        length = 0
         while p:
-            length+=1
-            p=p.next
+            length += 1
+            p = p.next
         return length
     
     def __iter__(self):
-        p=self.head.next
+        p=self.head
         it=[]
         while p:
             it.append(p.data)
-            p=p.next
+            p = p.next
         return iter(it)
     
-    def add_node(self,data,pos):
+    def add_node(self,node,pos):
         '''add node at the position pos starting from 0
         e.g 
         pos: 0 1 2 3 4 5
         list: 2 7 9 8 3
+        return  0 if success
+                1 if failed
         '''
-        if pos>len(self):
-            print 'Wrong position'        
-            return 
-        p=ListNode(data)
-        q=self.head
+        dummy = ListNode(0)
+        dummy.next = self.head
+        p = dummy
         for i in range(pos):
-            q=q.next
-        p.next=q.next
-        q.next=p
+            p = p.next
+            if not p:
+                print 'Wrong position'
+                return 1
         
-    def append_node(self,data):
-        self.add_node(data, len(self))
-    
+        node.next = p.next
+        p.next = node
+        return 0
+        
+    def append_node(self, node):
+        p = self.get_tail_node()
+        if not p:
+            self.head = node
+            return
+        p.next = node
+        
     def get_tail_node(self):
         p = self.head
-        if not p.next:
+        if not p:
             return None
         while p.next:
             p = p.next
         return p
         
-    def del_node_value(self,data):
+    def del_node_by_value(self,data):
         'delete all nodes with the certain @p data'
-        if not self.head or not self.head.next:
+        if not self.head:
             return
+        dummy = ListNode(0)
+        dummy.next = self.head
         
-        prev=self.head
-        curr=self.head.next
+        prev = dummy
+        curr = self.head
         
         while curr:
-            if curr.data==data:
+            if curr.data == data:
                 prev.next = curr.next
                 del curr
                 curr = prev.next
@@ -80,124 +91,142 @@ class LinkList(object):
                 prev = curr
                 curr = curr.next
     
-    def del_node_pos(self,pos):
+    def del_node_by_pos(self, pos):
         ''' delete ListNode at the position starting from 0
             e.g.
             pos:  0 1 2 3 4
             list: 2 7 9 8 3
+            return  0 if success
+                    1 if failed
         '''
-        if pos>len(self)-1:
-            raise IndexError(pos)
-        p=self.head
+        if not self.head:
+            return 1
+        if pos == 0:
+            self.head = self.head.next
+            return 0
+        dummy = ListNode(0)
+        dummy.next = self.head
+        p = dummy
         for i in range(pos):
-            p=p.next
-        q=p.next
-        p.next=q.next
+            p = p.next
+            if not p.next:
+                print 'pos out of range'
+                return 1
+        q = p.next
+        p.next = q.next
         del q
+        return 0
    
     def print_list(self):
-        current=self.head.next
-        while current:
-            print current.data,
-            current=current.next
+        curr = self.head
+        while curr:
+            print curr.data,
+            curr = curr.next
         print
         
     def reverse_list(self):
-        p=self.head.next
-        if p==None or p.next==None:
+        dummy = ListNode(0)
+        p = self.head
+        if not p or not p.next:
             return
-        self.head.next=None
         while p:
-            q=p.next    #record p's next
-            p.next=self.head.next   #the following two steps is equivalent to inserting p at position 0
-            self.head.next=p
+            q = p.next    #record p's next
+            p.next = dummy.next   #the following two steps is equivalent to inserting p at position 0
+            dummy.next=p
             #self.add_node(p.data, 0)
-            p=q
+            p = q
+        self.head = dummy.next
     
     def reverse_list_recur(self):
         if not self.head or not self.head.next:
-            return None
-        self.reverse_list_recur_helper(self.head.next)
+            return
+        self.reverse_list_recur_helper(self.head)
         
     def reverse_list_recur_helper(self, p):
         if not p.next:
             return p
         new_head = self.reverse_list_recur_helper(p.next)
-        p.next.next=p
-        p.next=None
-        self.head.next = new_head
+        p.next.next = p
+        p.next = None
+        self.head = new_head
         return new_head
     
     
-    def make_loop(self,pos):
+    def make_loop(self, pos):
         'Make the linked list a loop between pos and tail, pos starts from 0'
         if pos>len(self)-1:
             print 'Wrong position'        
             return 
-        tail=self.head.next
-        while tail.next:
-            tail=tail.next
-        p=self.head.next
+        tail = self.get_tail_node()
+        p = self.head
         for i in range(pos):
-            p=p.next
-        tail.next=p
+            p = p.next
+        tail.next = p
         
     def get_loop_point(self):
-        'Get the interjection of the loop point, i.e. the parameter pos of make_loop'
-        p=self.head.next.next
-        q=self.head.next
-        while p!=q:
-            p=p.next.next
-            q=q.next
-        q=self.head
-        while p!=q:
-            p=p.next
-            q=q.next
+        'Get the loop start point, i.e. the parameter pos of make_loop'
+        if not self.head:
+            return None
+        dummy = ListNode(0)
+        dummy.next = self.head
+        p = self.head.next
+        q = self.head
+        while p != q:
+            p = p.next.next
+            q = q.next
+        q = dummy
+        while p != q:
+            p = p.next
+            q = q.next
         return p
     
     def loop_length(self):
-        p=self.get_loop_point()
-        loopDict={}
-        length=0   
-        while p not in loopDict:
-            loopDict[p]=1
-            length+=1
-            p=p.next
-        return length
+        p = self.get_loop_point()
+        if not p:
+            return -1
+        q = p.next
+        loop_len = 1
+        while q != p:
+            q = q.next
+            loop_len += 1
+        return loop_len
         
 if __name__ == '__main__':
     b=LinkList([1,2,3])
     b.print_list()
     a=LinkList()
-    a.append_node(3)
+    a.append_node(ListNode(3))
     a.print_list()
     data=[5,5,8,5,5,5,9,10,3,5,5]
     for each_data in data:
-        a.append_node(each_data)
+        a.append_node(ListNode(each_data))
     print 'Test creating list'
     a.print_list()
     print 'List length is ', len(a)
     
     print 'Test adding node'
-    a.add_node(6,3)
+    a.add_node(ListNode(6), 3)
     a.print_list()
     print 'List length is ', len(a)
     
     print 'Test deleting node by value, deleting 5'
-    a.del_node_value(5)
+    a.del_node_by_value(5)
     a.print_list()
     print 'List length is ', len(a)
     
     print 'Test deleting node by position'
-    a.del_node_pos(0)
+    a.del_node_by_pos(0)
     a.print_list()
     print 'List length is ', len(a)
     
     print 'Test iterating'
     for d in a:
-        print d
+        print d,
+    print
     
     print 'Test reverse'
+    a.print_list()
+    a.reverse_list()
     a.print_list()
     a.reverse_list_recur()
     a.print_list()
@@ -206,3 +235,10 @@ if __name__ == '__main__':
     a.make_loop(1)
     print 'loop point is', a.get_loop_point().data
     print 'loop length is', a.loop_length()
+    
+    l1 = LinkList()
+    #l1.append_node(ListNode(1))
+    l1.print_list()
+    #l1.head.next = l1.head
+    print l1.get_loop_point()
+    print l1.loop_length()
--- LeetCode/sort_list.py
+++ LeetCode/sort_list.py
@@ -3,7 +3,7 @@
     Q2.
     Sort a linked list using merge sort
 '''
-from LinkList import LinkList
+from LinkList import *
 def insertion_sort_list(orig_list):
     head = orig_list.head
     if not head.next:
@@ -27,7 +27,37 @@ def insertion_sort_list(orig_list):
             q = q_pre.next
     return orig_list
 
-def merge_sort_list(orig_list):
+def merge_sort_list_1(head):
+    if not head or not head.next:
+        return head
+    dummy = ListNode(0)
+    dummy.next= head
+    slow = dummy
+    fast = dummy
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+    l1 = head
+    l2 = slow.next
+    slow.next = None
+    nh_1 = sortList(l1)
+    nh_2 = sortList(l2)
+    return merge(nh_1, nh_2)
+    
+def merge(l1, l2):
+    if not l1:
+        return l2
+    if not l2:
+        return l1
+    if l1.data <= l2.data:
+        l1.next = merge(l1.next, l2)
+        return l1
+    else:
+        l2.next = merge(l1, l2.next)
+        return l2
+
+
+def merge_sort_list_2(orig_list):
     # no element in list
     if not orig_list.head.next:
         return 
@@ -74,10 +104,15 @@ def merge_sort_list(orig_list):
         
         
 if __name__=='__main__':
-    test_cases = [[3,7,4,8,6,5],[10,7,4,11,5,2,6,19,1],[1],[5,4],[6,5,4,3,2,2],[10,4,7,11,5,11,2,4]]
+    test_cases = [[3,7,4,8,6,5],[10,7,4,11,5,2,6,19,1],[1],[5,4],[6,5,4,3,2,2],[10,4,7,11,5,11,2,4], []]
     for each_test_case in test_cases:
         orig_list = LinkList(each_test_case)
-        insertion_sort_list(orig_list).print_list()
-        orig_list = LinkList(each_test_case)
+        h = sortList(orig_list.head.next)
+        while h:
+            print h.data,
+            h = h.next
+        print
+        #insertion_sort_list(orig_list).print_list()
+        #orig_list = LinkList(each_test_case)
         #merge_sort_list(orig_list).print_list()
         
